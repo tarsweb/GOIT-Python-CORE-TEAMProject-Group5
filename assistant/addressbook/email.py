6f66c9260475b26b82946b1c7799c417c1fc32ed
tarsweb <58361241+tarsweb@users.noreply.github.com>
2024-01-13 22:43:30 +0200

    validation

--- assistant/addressbook/email.py
+++ assistant/addressbook/email.py
@@ -1,8 +1,9 @@
 from .fields import Field
+import re
 
 
 class Email(Field):
-    def __init__(self, email: str):
+    def __init__(self, email: str) -> None:
         super().__init__(self.__is_valid_email(email))
 
     @property
@@ -10,14 +11,43 @@ class Email(Field):
         return self.value
 
     @email.setter
-    def email(self, value):
-        self.value = self.__is_valid_email(value)
+    def email(self, email: str):
+        self.value = self.__is_valid_email(email)
 
     @staticmethod
-    def __is_valid_email(email: str):
-        # if not email:
-        #     raise ValueError("Email is required")
-        return email
+    def __is_valid_email(email: str) :
+        email_pattern = re.compile(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$")
+        if isinstance(email, str):
+            match_object = email_pattern.match(email)
+            print(match_object)
+            if match_object is not None:
+                return match_object.group()
+            else:
+                raise ValueError ("Invalid email")
+        else:
+            raise ValueError ("Invalid email")
 
-    def __str__(self):
-        return "unknown" if self.email is None else str(self.email)
+
+# from .fields import Field
+
+
+# class Email(Field):
+#     def __init__(self, email: str):
+#         super().__init__(self.__is_valid_email(email))
+
+#     @property
+#     def email(self):
+#         return self.value
+
+#     @email.setter
+#     def email(self, value):
+#         self.value = self.__is_valid_email(value)
+
+#     @staticmethod
+#     def __is_valid_email(email: str):
+#         # if not email:
+#         #     raise ValueError("Email is required")
+#         return email
+
+#     def __str__(self):
+#         return "unknown" if self.email is None else str(self.email)
