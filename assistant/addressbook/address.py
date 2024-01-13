f1fe471354fd87dc4b3d299932a5fc13f2b0a612
tarsweb <58361241+tarsweb@users.noreply.github.com>
2024-01-13 13:20:39 +0200

    add implementation class

--- assistant/addressbook/address.py
+++ assistant/addressbook/address.py
@@ -1,4 +1,16 @@
 from .fields  import Field
 
 class Address(Field):
-    pass
+    def __init__(self, address: str):
+        super().__init__(address)
+
+    @property
+    def address(self):
+        return self.value
+
+    @address.setter
+    def email(self, value):
+        self.value = value
+
+    def __str__(self):
+        return "-" if self.address is None else str(self.address)
