82743cddb43839cd229d0715e99a6062f3b21f23
tarsweb <58361241+tarsweb@users.noreply.github.com>
2024-01-13 13:21:12 +0200

    add implementation class

--- assistant/addressbook/email.py
+++ assistant/addressbook/email.py
@@ -1,4 +1,23 @@
-from .fields  import Field
+from .fields import Field
+
 
 class Email(Field):
-    pass
+    def __init__(self, email: str):
+        super().__init__(self.__is_valid_email(email))
+
+    @property
+    def email(self):
+        return self.value
+
+    @email.setter
+    def email(self, value):
+        self.value = self.__is_valid_email(value)
+
+    @staticmethod
+    def __is_valid_email(email: str):
+        # if not email:
+        #     raise ValueError("Email is required")
+        return email
+
+    def __str__(self):
+        return "unknown" if self.email is None else str(self.email)
