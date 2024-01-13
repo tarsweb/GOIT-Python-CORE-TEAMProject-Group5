e78f28750fd84270ed2a39ad6c69d3743fb929bc
Georgij38 <georgijzaharov85@gmail.com>
2024-01-13 19:29:12 +0200

    email valid

--- assistant/addressbook/email.py
+++ assistant/addressbook/email.py
@@ -1,4 +1,29 @@
-from .fields  import Field
+from fields import Field
+import re
 
 class Email(Field):
-    pass
+    def __init__(self, email: str) -> None:
+        super().__init__(self.__is_valid_email(email))
+
+    @property
+    def email(self):
+        return self.value
+
+    @email.setter
+    def email(self, email: str):
+        self.value = self.__is_valid_email(email)
+
+    email_pattern = re.compile(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$')
+
+    def __is_valid_email(self, email):
+        if isinstance(email, str):
+            match_object = self.email_pattern.match(email)
+            print(match_object)
+            if match_object is not None:
+                print(0)
+                return match_object.group()
+            else:
+                print('Invalid email')
+        else:
+            print('Invalid email')
+
