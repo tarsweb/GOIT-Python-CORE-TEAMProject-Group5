4bacc0e6339c255e4d67a9f0ad21c282bbec896c
tarsweb <58361241+tarsweb@users.noreply.github.com>
2024-01-13 13:22:10 +0200

    add implementation class

--- assistant/addressbook/record.py
+++ assistant/addressbook/record.py
@@ -1,9 +1,70 @@
 from .name import Name
+from .email import Email
+from .address import Address
+from .birthday import Birthday
+from .phone import Phone
+
+from datetime import date
 
 
 class Record:
-    def __init__(self, name: str):
+    def __init__(
+        self, name: str, address: str = None, email: str = None, birthday: str = None
+    ):
         self.name = Name(name)
+        self.address = Address(address)
+        self.email = Email(email)
+        self.birthday = Birthday(birthday)
+        self.phones = []
+
+    def add_phone(self, phone_number: str) -> None:
+        self.phones.append(Phone(phone_number))
+
+    def remove_phone(self, phone_number: str) -> None:
+        self.phones = list(
+            filter(lambda phone: phone.value != phone_number, self.phones)
+        )
+
+    def edit_phone(self, old_phone_number: str, new_phone_number: str) -> None:
+        phone_record = self.find_phone(old_phone_number)
+        if phone_record is None:
+            raise ValueError("Phone {old_phone_number} not found")
+        phone_record.phone = new_phone_number
+
+    def find_phone(self, phone_number) -> Phone | None:
+        phones_find = tuple(
+            filter(lambda phone: phone.phone == phone_number, self.phones)
+        )
+        if len(phones_find):
+            return phones_find[0]
+
+    def add_birthday(self, day_birthday: str) -> None:
+        self.birthday.birthday_date = day_birthday
+
+    def remove_birthday(self) -> None:
+        self.birthday.birthday_date = None
+
+    def edit_birthday(self, day_birthday: str) -> None:
+        self.birthday.birthday_date = day_birthday
+
+    def days_to_birthday(self) -> int:
+        if self.birthday.birthday_date is not None:
+            current_day = date.today()
+            offset_year = 0
+            month_birthday, day_birthday = (
+                self.birthday.birthday_date.month,
+                self.birthday.birthday_date.day,
+            )
+            if (current_day.month > month_birthday) or (
+                current_day.month == month_birthday and current_day.day > day_birthday
+            ):
+                offset_year = 1
+
+            current_birthday = self.birthday.birthday_date.replace(
+                year=current_day.year + offset_year
+            )
+
+            return (current_birthday - current_day).days
 
     def __str__(self) -> str:
-        return f"{self.name}"
+        return f"Contact name: {self.name} address: {self.address} email: {self.email} birthday: {self.birthday} phones: {'; '.join(p.value for p in self.phones)}"
