b44d57e738652dc29f65f9552930c18d39c6deab
tarsweb <58361241+tarsweb@users.noreply.github.com>
2024-01-16 01:49:03 +0200

    validation

--- assistant/addressbook/birthday.py
+++ assistant/addressbook/birthday.py
@@ -1,10 +1,11 @@
-from datetime import date
+import re
+from datetime import date, datetime
 from .fields import Field
 
 
 class Birthday(Field):
     def __init__(self, birthday: str) -> None:
-        super().__init__(self.__is_valid_birthday(birthday))
+        super().__init__(self.is_valid_birthday(birthday))
 
     @property
     def birthday_date(self):
@@ -12,17 +13,32 @@ class Birthday(Field):
 
     @birthday_date.setter
     def birthday_date(self, birthday: str):
-        self.value = self.__is_valid_birthday(birthday)
+        self.value = self.is_valid_birthday(birthday)
 
     @staticmethod
-    def __is_valid_birthday(birthday: str) -> date:
+    def is_valid_birthday(birthday: str) -> date | Exception:
+        def parse_date(date_string):
+            parts = re.split(r"[ \-,.]", date_string)
+            current_date = datetime.now()
+
+            if len(parts[0]) == 4:
+                year, month, day = parts
+            else:
+                day, month, year = parts
+            birthdate = datetime(int(year), int(month), int(day))
+            if current_date <= birthdate:
+                raise ValueError("Date of birth is greater than the current date")
+
+            return birthdate.date()
+
         if not birthday is None:
-            try:
-                date_list_of_birthday = [int(i) for i in birthday.split(".")]
-                date_birthday = date(*date_list_of_birthday)
-                return date_birthday
-            except Exception:  # Exception on any types
-                raise ValueError("birthday must have YYYY.MM.DD format!")
+            pattern = re.compile(
+                r"^(\d{4}\.\d{1,2}\.\d{1,2}|\d{1,2}\.\d{1,2}\.\d{4})$"
+            )
+            if not pattern.match(birthday) :
+                raise ValueError("The date format must match (yyyy.mm.dd) or (dd.mm.yyyy)")
+
+            return parse_date(birthday)
 
     def __str__(self):
         return "unknown" if self.birthday_date is None else str(self.birthday_date)
