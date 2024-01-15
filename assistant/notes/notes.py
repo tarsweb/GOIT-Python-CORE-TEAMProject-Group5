71156e6fd941e78d6161a8d5b3eb32b014eb34b3
kbuhantsev <k.buhantsev@gmail.com>
2024-01-15 21:39:16 +0200

    notes saving

--- assistant/notes/notes.py
+++ assistant/notes/notes.py
@@ -1,4 +1,6 @@
 from collections import UserList
+from contextlib import suppress
+import json
 from .record import Record
 
 
@@ -17,10 +19,37 @@ class Note(UserList):
         pass
 
     def save_data(self):
-        pass
+        database = {}
+        with suppress(FileNotFoundError):
+            with open(file="db.json", mode="r", encoding="utf8") as file:
+                text = file.read()
+                database = json.loads(text)
+        for record_data in self.data:
+            with suppress(AttributeError):
+                record = {
+                    "text": record_data["text"],
+                    "tags": record_data["tags"],
+                }
+                database["notes"].append(record)
+        with open(file="db.json", mode="w", encoding="utf8") as file:
+            text = json.dumps(database)
+            file.write(text)
+
 
     def load_data(self):
-        pass
+        with suppress(FileNotFoundError):
+            with open(file="db.json", mode="r", encoding="utf8") as file:
+                text = file.read()
+                data = json.loads(text)
+                if data:
+                    for name, record_data in data["book"].items():
+                        record = Record(name)
+                        record.add_address(record_data["address"])
+                        record.add_email(record_data["email"])
+                        record.add_birthday(record_data["birthday"])
+                        for phone in record_data["phones"]:
+                            record.add_phone(phone)
+                        self.data[name] = record
 
     def __iter__(self):
         return iter(self.data)
