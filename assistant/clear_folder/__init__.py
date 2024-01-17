2cec73f98e430a83ec3e12275d1627b693a90275
tarsweb <58361241+tarsweb@users.noreply.github.com>
2024-01-17 21:47:40 +0200

    fix bug

--- assistant/clear_folder/__init__.py
+++ assistant/clear_folder/__init__.py
@@ -1,9 +1,12 @@
+from pathlib import Path
+from cli_utils import register, print_clear_folder, get_warning_message
 from .clearfolder import clearfolder
-from cli_utils import register, print_clear_folder
 
 
 def initialize():
     @register("clear-folder", "cleaner")
     def clean(path_to_dir: str) -> str:
+        if not Path(path_to_dir).exists():
+            return get_warning_message(clean.__name__, f"Folder `{path_to_dir}` not exist")
         result = clearfolder(path_to_dir)
         return print_clear_folder(result)
