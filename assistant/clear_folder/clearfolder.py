4d9424a9f714cba5c83698b05bf0b22c673a3a26
tarsweb <tarsweb@ukr.net>
2024-01-18 19:23:48 +0200

    fix clearfolder

--- assistant/clear_folder/clearfolder.py
+++ assistant/clear_folder/clearfolder.py
@@ -1,92 +1,112 @@
 import os
-import re
 import shutil
-import zipfile
+
+from assistant.cli_utils import get_success_message, get_error_message, get_warning_message
+
+SYMBOLS = """ !"#$%&'()*+№,-/:;<=>?@[\\]^`{|}~"""
+CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
+TRANSLATION = (
+    "a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+    "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g"
+)
+TRANS = {}
+for c, l in zip(CYRILLIC_SYMBOLS, TRANSLATION):
+    TRANS[ord(c)] = l
+    TRANS[ord(c.upper())] = l.upper()
+for symbol in SYMBOLS:
+    TRANS[ord(symbol)] = "_"
 
 
-def normalize(name):
-    translit_dict = {
-        "а": "a",
-        "б": "b",
-        "в": "v",
-        "г": "h",
-        "ґ": "g",
-        "д": "d",
-        "е": "e",
-        "є": "ye",
-        "ж": "zh",
-        "з": "z",
-        "и": "y",
-        "і": "i",
-        "ї": "i",
-        "й": "i",
-        "к": "k",
-        "л": "l",
-        "м": "m",
-        "н": "n",
-        "о": "o",
-        "п": "p",
-        "р": "r",
-        "с": "s",
-        "т": "t",
-        "у": "u",
-        "ф": "f",
-        "х": "kh",
-        "ц": "ts",
-        "ч": "ch",
-        "ш": "sh",
-        "щ": "shch",
-        "ь": "",
-        "ю": "iu",
-        "я": "ia",
-        "А": "A",
-        "Б": "B",
-        "В": "V",
-        "Г": "H",
-        "Ґ": "G",
-        "Д": "D",
-        "Е": "E",
-        "Є": "Ye",
-        "Ж": "Zh",
-        "З": "Z",
-        "И": "Y",
-        "І": "I",
-        "Ї": "I",
-        "Й": "I",
-        "К": "K",
-        "Л": "L",
-        "М": "M",
-        "Н": "N",
-        "О": "O",
-        "П": "P",
-        "Р": "R",
-        "С": "S",
-        "Т": "T",
-        "У": "U",
-        "Ф": "F",
-        "Х": "Kh",
-        "Ц": "Ts",
-        "Ч": "Ch",
-        "Ш": "Sh",
-        "Щ": "Shch",
-        "Ь": "",
-        "Ю": "Iu",
-        "Я": "Ya",
-    }
+FILE_TYPES = {
+    "images": ["JPEG", "PNG", "JPG", "SVG", "BMP"],
+    "video": ["AVI", "MP4", "MOV", "MKV"],
+    "documents": ["DOC", "DOCX", "TXT", "PDF", "XLS", "XLSX", "PPTX"],
+    "audio": ["MP3", "OGG", "WAV", "AMR"],
+    "archives": ["ZIP", "GZ", "TAR", "RAR"],
+}
 
-    translation_table = str.maketrans(translit_dict)
-    name = name.translate(translation_table)
-    name = re.sub(r"[^A-Za-z0-9.]+", "_", name)
+FOLDER_ARCHIVE = ("archives",)
+FOLDER_UNKNOWN = "unknown"
 
-    return name
+files_by_type = {}
+
+result_work = {
+    "error": (),
+    "remove_folder": (),
+    "unknown_extension": (),
+    "message": (),
+}
 
 
-def move_files(source, destination):
-    for root, _, files in os.walk(source):
-        for file in files:
-            file_path = os.path.join(root, file)
-            destination_path = os.path.join(destination, file)
-            shutil.move(file_path, destination_path)
+def get_extention_file(path_to_file: str) -> str:
+    return os.path.splitext(path_to_file)[1].replace(".", "")
+
+
+def find_type_files(folder_path: str, type_files: list) -> list:
+    return list(
+        os.path.join(folder_path, item)
+        for item in os.listdir(folder_path)
+        if os.path.isfile(os.path.join(folder_path, item))
+        and get_extention_file(item).upper() in type_files
+    )
+
+
+def unknown_files(folder_path: str, known_files: list) -> list:
+    return list(
+        os.path.join(folder_path, item)
+        for item in os.listdir(folder_path)
+        if os.path.isfile(os.path.join(folder_path, item))
+        and os.path.join(folder_path, item) not in known_files
+    )
+
+
+def subfolder_list(folder_path: str, ignore_path: tuple = tuple()) -> list:
+    return [
+        item
+        for item in os.listdir(folder_path)
+        if os.path.isdir(os.path.join(folder_path, item)) and item not in ignore_path
+    ]
+
+
+def normalize(string: str) -> str:
+    return string.translate(TRANS)
+
+
+def move_files(destination: str, file_list: list, unpack=False) -> None:
+    for file in file_list:
+        name_file, extention = os.path.splitext(os.path.basename(file))
+        file_name = normalize(name_file)
+        new_file_path = os.path.join(destination, "".join((file_name, extention)))
+        counter_bad_name = 0
+        while True:
+            if not os.path.exists(new_file_path):
+                if counter_bad_name != 0:
+                    file_name = f"{file_name}-{counter_bad_name}"
+                break
+            else:
+                maby_file_name = f"{file_name}-{counter_bad_name + 1}"
+                new_file_path = os.path.join(
+                    destination, "".join((maby_file_name, extention))
+                )
+                counter_bad_name += 1
+        try:
+            shutil.move(file, new_file_path)
+        except FileExistsError:
+            update_result_work("error", f"{file} replace error")
+
+        if unpack:
+            try:
+                shutil.unpack_archive(
+                    new_file_path, os.path.join(destination, file_name)
+                )
+                # Remove
+                os.remove(new_file_path)
+            except shutil.ReadError:
+                update_result_work(
+                    "error", f"Find archive {new_file_path} unpack error"
+                )
+            except FileNotFoundError:
+                update_result_work("error", f"File {new_file_path} not found")
 
 
 def remove_empty_folders(folder):
@@ -95,149 +115,126 @@ def remove_empty_folders(folder):
             dir_path = os.path.join(root, i)
             if not os.listdir(dir_path):
                 os.rmdir(dir_path)
+                update_result_work("remove_folder", dir_path)
 
 
-def scan_folder(folder_path):
-    file_types = {
-        "images": [
-            "jpeg",
-            "png",
-            "jpg",
-            "svg",
-            "bmp",
-            "JPEG",
-            "PNG",
-            "JPG",
-            "SVG",
-            "BMP",
-        ],
-        "video": ["avi", "mp4", "mov", "mkv", "AVI", "MP4", "MOV", "MKV"],
-        "documents": [
-            "doc",
-            "docx",
-            "txt",
-            "pdf",
-            "xls",
-            "xlsx",
-            "pptx",
-            "DOC",
-            "DOCX",
-            "TXT",
-            "PDF",
-            "XLS",
-            "XLSX",
-            "PPTX",
-        ],
-        "audio": ["mp3", "ogg", "wav", "amr", "MP3", "OGG", "WAV", "AMR"],
-        "archives": ["zip", "gz", "tar", "rar", "ZIP", "GZ", "TAR", "RAR"],
-    }
+def scan_folder(folder_path, ignore_path: tuple = tuple()):
+    files_by_type_current_folder = {}
 
-    files_by_type = {category: [] for category in file_types}
-    unknown_extensions = []
+    for category, type_files in FILE_TYPES.items():
+        file_by_category = find_type_files(folder_path, type_files)
+        files_by_type[category] += file_by_category
+        files_by_type_current_folder.setdefault(category, file_by_category)
 
-    for item in os.listdir(folder_path):
-        item_path = os.path.join(folder_path, item)
+    list_known = []
+    _ = [list_known.extend(v) for k, v in files_by_type.items()]
 
-        if os.path.isfile(item_path):
-            extension = item.split(".")[-1]
-            added_to_category = False
+    files_by_type["unknown"] += unknown_files(folder_path, list_known)
 
-            for category, extensions in file_types.items():
-                if extension.lower() in extensions:
-                    normalized_name = normalize(item.split(".")[0]) + "." + extension
-                    new_item_path = os.path.join(folder_path, normalized_name)
-
-                    if item_path != new_item_path:
-                        if os.path.exists(new_item_path):
-                            added_to_category = True
-                            break
-
-                        os.rename(item_path, new_item_path)
-
-                    files_by_type[category].append(normalized_name)
-                    added_to_category = True
-                    break
-
-            if not added_to_category:
-                unknown_extensions.append(item)
-
-        elif os.path.isdir(item_path):
-            subfolder_files, subfolder_unknown = scan_folder(item_path)
-
-            for category, files in subfolder_files.items():
-                files_by_type[category].extend(files)
-
-            unknown_extensions.extend(subfolder_unknown)
-
-            normalized_name = normalize(item)
-            new_item_path = os.path.join(folder_path, normalized_name)
-
-            if item_path != new_item_path:
-                if os.path.exists(new_item_path):
-                    continue
-
-                os.rename(item_path, new_item_path)
-
-    return files_by_type, unknown_extensions
+    for item in subfolder_list(folder_path, ignore_path):
+        scan_folder(os.path.join(folder_path, item))
 
 
-def move_unknown_files(unknown_files, destination_path):
-    unknown_files_path = os.path.join(destination_path, "unknown_files")
-    os.makedirs(unknown_files_path, exist_ok=True)
+def file_handlers(folder_result_path):
+    for category, file_list in files_by_type.items():
+        if len(file_list) == 0:
+            continue  ## skip if not files
 
-    for file in unknown_files:
-        file_path = os.path.join(destination_path, file)
-        new_file_path = os.path.join(unknown_files_path, file)
-        shutil.move(file_path, new_file_path)
+        destination_path = os.path.join(folder_result_path, category)
+        if not os.path.exists(destination_path):
+            os.mkdir(destination_path)
+        move_files(destination_path, file_list, unpack=category in FOLDER_ARCHIVE)
 
 
-def organize_files(files, destination_path, unsupported: []) -> None:
-    for category, file_list in files.items():
-        if category == "archives":
-            category_path = os.path.join(destination_path, category)
-            os.makedirs(category_path, exist_ok=True)
+def update_result_work(result_key: str, *args) -> None:
+    global result_work
+    if result_key in result_work:
+        result_work[result_key] = (*result_work[result_key], *args)
 
-            for file in file_list:
-                file_path = os.path.join(destination_path, file)
-                archive_name = os.path.splitext(file)[0]
-                archive_path = os.path.join(category_path, archive_name)
-                os.makedirs(archive_path, exist_ok=True)
 
-                if zipfile.is_zipfile(file_path):
-                    with zipfile.ZipFile(file_path, "r") as zip_ref:
-                        zip_ref.extractall(archive_path)
-                    os.remove(file_path)
-                else:
-                    unsupported.append(file)
+def get_result_work(folder_result_path, ignore_path: tuple = tuple()) -> list:
+    global result_work
 
-        elif category in ["video", "audio", "documents", "images"]:
-            category_path = os.path.join(destination_path, category)
-            os.makedirs(category_path, exist_ok=True)
+    used_extension = ()
 
-            for file in file_list:
-                file_path = os.path.join(destination_path, file)
-                new_file_path = os.path.join(category_path, file)
-                shutil.move(file_path, new_file_path)
+    for i in ignore_path:
+        current_path = os.path.join(folder_result_path, i)
+        if not os.path.exists(current_path):
+            continue
 
+        in_folder = list(item for item in os.listdir(current_path))
+        if i == FOLDER_UNKNOWN:
+            update_result_work(
+                "unknown_extension",
+                *tuple(
+                    get_extention_file(item).upper()
+                    for item in in_folder
+                    if os.path.isfile(os.path.join(current_path, item))
+                ),
+            )
         else:
-            unsupported.append(category)
-
-
-def clearfolder(folder_path):
-    destination_path = folder_path
-    files, unknown_extensions = scan_folder(folder_path)
-    unsupported = []
-
-    move_files(folder_path, destination_path)
-    remove_empty_folders(folder_path)
-    organize_files(files, destination_path, unsupported)
-    move_unknown_files(unknown_extensions, destination_path)
+            used_extension = (
+                *used_extension,
+                *set(
+                    get_extention_file(item).upper()
+                    for item in in_folder
+                    if os.path.isfile(os.path.join(current_path, item))
+                ),
+            )
+            update_result_work(
+                "message",
+                f"The folder '{i}' contains : {', '.join(in_folder)}",
+            )
 
     result = []
-    for category, file_list in files.items():
-        result.append({category: ", ".join(file_list)})
-    result.append({"unknown": ", ".join(unknown_extensions)})
-    if len(unsupported) > 0:
-        result.append({"unsupported": ", ".join(unsupported)})
+    for k, v in result_work.items():
+        if k == "error":
+            if len(v) != 0:
+                result.append({"Error": "\n".join([get_error_message(i) for i in v])})
+        elif k == "remove_folder":
+            if len(v) != 0:
+                result.append({"Remove folder": get_warning_message("", ", ".join(v))})
+        elif k == "unknown_extension":
+            if len(v) != 0:
+                result.append(
+                    {
+                        "Unknown extension": get_warning_message(
+                            "", " ".join(set(v)).strip()
+                        )
+                    }
+                )
+        else:
+            if len(v) != 0:
+                result.append(
+                    {"Statistics": "\n".join([get_success_message(i) for i in v])}
+                )
+
+    if len(used_extension) > 0:
+        result.append({"Used extension": get_success_message(" ".join(used_extension))})
 
     return result
+
+
+def clearfolder(folder_path, folder_result: str = None):
+    global files_by_type
+    global result_work
+
+    folder_result = folder_path if folder_result is None else folder_result
+
+    files_by_type = {category: [] for category in FILE_TYPES}
+    files_by_type.setdefault(FOLDER_UNKNOWN, [])
+
+    result_work = dict.fromkeys(result_work, tuple())
+
+    if folder_path == folder_result:
+        ignore_path = tuple(FILE_TYPES)
+    else:
+        ignore_path = tuple()
+
+    scan_folder(folder_path, ignore_path)
+
+    file_handlers(folder_result)
+
+    remove_empty_folders(folder_path)
+
+    return get_result_work(folder_result, tuple([*list(FILE_TYPES), FOLDER_UNKNOWN]))
